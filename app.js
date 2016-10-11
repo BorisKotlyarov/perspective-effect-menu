5ee37ec933a465f24cc09d7f0a04c1490bbdf753
Boris Kotlyarov <9309706@gmail.com>
2016-10-11 18:51:49 +0300

    background was edited

--- app.js
+++ app.js
@@ -1,29 +1,32 @@
-function el(selector) {
-    return document.querySelector(selector);
-}
+(function () {
+    'use strict';
+    function el(selector) {
+        return document.querySelector(selector);
+    }
 
-function open(){
-    el(".container").classList.toggle("perspective");
-    setTimeout(function(){
-        el(".menu-block").classList.toggle("open");
-    }, 200);
-}
+    function open(){
+        el(".container").classList.toggle("perspective");
+        setTimeout(function(){
+            el(".menu-block").classList.toggle("open");
+        }, 200);
+    }
 
-function close(){
-    el('.menu-block').classList.remove('open');
-    setTimeout(function(){
-        el('.container').classList.remove("perspective");
-    }, 200);
-}
+    function close(){
+        el('.menu-block').classList.remove('open');
+        setTimeout(function(){
+            el('.container').classList.remove("perspective");
+        }, 200);
+    }
 
-function toggler(){
-    el('.left-block').addEventListener('click',function(){
-        if (el('.container').classList.contains("perspective")) {
-            close()
-        } else {
-            open();
-        }
-    });
-}
+    function toggler(){
+        el('.left-block').addEventListener('click',function(){
+            if (el('.container').classList.contains("perspective")) {
+                close()
+            } else {
+                open();
+            }
+        });
+    }
 
-window.addEventListener('load', toggler);
+    window.addEventListener('load', toggler);
+})();
